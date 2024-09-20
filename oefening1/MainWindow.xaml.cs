8102351b515726336b2f1f6efabef0468d6f5545
alexander100100 <alexander.mahrhofer@student.pxl.be>
2024-09-20 10:39:55 +0200

    Add project files.

---
+++ oefening1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace oefening1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // global variables
+        // constanten
+        const double EXCHANGE_RATE = 40.3399;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, RoutedEventArgs e)
+        { // inlezen user value
+            double euro = double.Parse(euroTextBox.Text);
+
+            // converten
+            double bef = euro * EXCHANGE_RATE;
+
+            // uitlezen
+            BEFTextBox.Text = bef.ToString("F2");
+        }
+
+        private void wissenButton_Click(object sender, RoutedEventArgs e)
+        {
+            // alle textboxen terug op '0' zetten
+            euroTextBox.Text = 0.ToString();
+            BEFTextBox.Text = 0.ToString();
+        }
+    }
+}
+
+        
